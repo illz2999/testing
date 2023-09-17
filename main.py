1ecb6ad198972cdf2be2d7c9081460b26a11d4e0
DeNic842 <denic842@meta.ua>
2023-09-17 14:00:17 +0300

    Crete proiect

---
+++ main.py
@@ -0,0 +1,19 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (QVBoxLayout, QHBoxLayout,QLabel, QPushButton,QApplication,QWidget)
+
+
+app = QApplication([])
+main_win = QWidget
+main_win. setWindowTitle("ЕЕСтуваня")
+main_win.resizen(500,800)
+
+main_win.show()
+app.exec_()
+
+
+
+
+
+
+
+
